c57c7692e7e38607b2ae683779ce866b13f4f10b
Ahany42 <ahanyfathy42@gmail.com>
2024-07-25 22:54:18 +0300

    Styling

--- src/Components/Comment/Comment.jsx
+++ src/Components/Comment/Comment.jsx
@@ -3,8 +3,10 @@ import './Comment.css';
 const Comment = ()=>{
     return(
         <div className='Comment' >
-            <h2 className="CommentText">This is Comment Text</h2>
              <Avatar/>
+            <h4 className="UserName">Aly Hany</h4>
+            <p className="CommentText">Lorem ipsum dolor sit amet consectetur adipisicing elit</p>
+            <h6 className="Date">26 July 2024</h6>
           </div>
     )
 }
