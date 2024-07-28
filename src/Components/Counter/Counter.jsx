b398b890eabf04a6dba3ad78c34524db9fab7f17
Ahany42 <ahanyfathy42@gmail.com>
2024-07-28 20:30:58 +0300

    useState Enhanced

--- src/Components/Counter/Counter.jsx
+++ src/Components/Counter/Counter.jsx
@@ -2,11 +2,12 @@ import {useState} from 'react';
 import './Counter.css';
 const Counter = () => {
     const [count,setCount]=useState(0);
+    const counterHandler=()=>setCount(count+1);
   return (
     <div className="CounterContainer">
       <h2>Count : {count}</h2>
 
-      <button onClick={()=> setCount(count+1)}>Increment</button>
+      <button onClick={counterHandler}>Increment</button>
     </div>
   )
 }
