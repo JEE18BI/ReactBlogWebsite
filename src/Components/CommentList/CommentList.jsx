6cae702acc184bb1b14814aa7f327e5f37f0bc61
Ahany42 <ahanyfathy42@gmail.com>
2024-08-11 22:02:46 +0300

    Placeholder Image

--- src/Components/CommentList/CommentList.jsx
+++ src/Components/CommentList/CommentList.jsx
@@ -1,11 +1,13 @@
 import Comment from '../Comment/Comment.jsx';
+import placeholderImg from '../../Images/Placeholder.svg';
 import './CommentList.css';
 const CommentList = ({data , deleteFunction}) => {
   if(data.length===0){
     console.log("zero") 
  return (
-   <div className="CommentList">
- <h3>No Data To Show</h3>
+   <div className="PlaceholderDiv">
+ <img src={placeholderImg} className="PlaceholderImage" alt="Placeholder image"/>
+ <h5>No Comments To Show</h5>
   </div>
  )
   }
