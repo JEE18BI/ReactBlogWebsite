2b05d57ded5d372ea6822faf832d6203ee62df95
Ahany42 <ahanyfathy42@gmail.com>
2024-08-16 15:07:14 +0300

    Fetching Comments From Api

--- src/App.jsx
+++ src/App.jsx
@@ -4,43 +4,20 @@ import {useState,useEffect} from 'react';
 import InputForm from './Components/InputForm/InputForm.jsx';
 import SearchBar from './Components/SearchBar/SearchBar.jsx';
 import '../src/App.css';
-const data = [
-  {
-    id: 1,
-    UserName: "Aly Hany",
-    Comment: "This is a great post!",
-    Date: "Sep 25,2024",
-    src:img1,
-    Gender:"M"
-  },
-  {
-    id: 2,
-    UserName: "Bob",
-    Comment: "I found this article very informative.",
-    Date: "Sep 26,2024",
-    Gender:"M"
-  },
-  {
-    id: 3,
-    UserName: "Charlie",
-    Comment: "I disagree with some points mentioned.",
-    Date: "Sep 27,2024",
-    Gender:"M"
-  },
-  {
-    id: 4,
-    UserName: "Diana",
-    Comment: "Can you provide more details on this topic?",
-    Date: "Aug 04,2024",
-    Gender:"F"
-  }
-];
-
 
 export function App() {
-  const [comments, setComments] = useState(data);
-  const [FilterData,setFilterData]=useState(comments);
+
+  const [comments, setComments] = useState("");
+  const [FilterData,setFilterData]=useState("");
   const [user,setUser]=useState("");
+  useEffect(()=>{
+    fetch("http://localhost:3004/Comments")
+    .then((data)=> data.json())
+    .then((fetchedData)=> {
+      setComments(fetchedData)
+      setFilterData(fetchedData)
+    }).catch((error) => console.error('Error fetching data:', error));
+  },[])
   useEffect(()=>{
     fetch("http://localhost:3004/LoggedInUser")
     .then((data)=> data.json())
@@ -56,6 +33,7 @@ export function App() {
   const addComment=(newComment)=>{
     setComments(prevState => (prevState,newComment));
   }
+  console.log(FilterData);
   return(
    <div className="App">
     <Header src={user.src}/>
