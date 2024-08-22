45af40b76445f55dcbf0dbb8b84425a57ad422d2
Ahany42 <ahanyfathy42@gmail.com>
2024-08-22 14:52:02 +0300

    Header MobileNavBar initial

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,6 +1,13 @@
 import Avatar from '../Avatar/Avatar.jsx'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
 import './Header.css';
 const Header = ({src}) => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const MobileNavBarHandler = ()=>{
+    setIsMenuOpen(!isMenuOpen);
+  }
   return (
     <div className="Header">
       <li className="NavBar">
@@ -11,6 +18,11 @@ const Header = ({src}) => {
         <ul>Login</ul>
       
       </li>
+      <div className="MobileNavBar">
+     <li>
+     <FontAwesomeIcon icon={isMenuOpen ? faTimes : faBars} color='white'style={{ color: 'white', fontSize: '24px' }} onClick={MobileNavBarHandler}/>
+     </li>
+      </div>
       <Avatar src={src}/>
     </div>
   )
