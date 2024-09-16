aae666c335a13a1dfb839cceaffb2fe25843943c
Ahany42 <ahanyfathy42@gmail.com>
2024-09-16 19:59:31 +0300

    Edit Reply Completed

--- src/Components/Reply/Reply.jsx
+++ src/Components/Reply/Reply.jsx
@@ -1,12 +1,14 @@
 import { useEffect,useState,useContext } from 'react';
 import { ApiContext } from '../../App';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPen, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { faPen , faTrash  } from '@fortawesome/free-solid-svg-icons';
 import Avatar from '../Avatar/Avatar';
-import '../Comment/Comment.css';
-import './Reply.css'
+import { ToastContainer, toast } from 'react-toastify';
+import './Reply.css';
 const Reply = ({reply,loggedInUser,comment,setReplies,setComment}) => {
     const [user,setUser] = useState("");
+    const [replyInput,setReplyInput] = useState(reply.reply);
+    const [inputForm,setInputForm] = useState(false);
     const {baseUrl} = useContext(ApiContext);
     useEffect(()=>{
         fetch(`${baseUrl}/users/get/${reply.postedBy}`)
@@ -18,36 +20,47 @@ const Reply = ({reply,loggedInUser,comment,setReplies,setComment}) => {
     if (!reply || !reply.postedBy) {
         return <div>Loading...</div>;
       }     
-      const EditReply = (replyId,commentId)=>{
-        if(commentId && replyId){
-          const updatedReply= {
-            reply: "Updated Reply"
-          };
-            fetch(`${baseUrl}/comments/${commentId}/replies/edit/${replyId}`, {
-              method: 'PATCH',
-              headers: {
-              'Content-Type': 'application/json'    
-              },
-              body: JSON.stringify(updatedReply)
-          })
-          .then(response => {
-            if (!response.ok) {
-              throw new Error(`Error: ${response.status}`);
-            }
-            return response.json();
-          })
-          .then(data => {
-          setReplies(prevReplies =>
-            prevReplies.map(reply =>
-              reply.id === replyId
-                ? { ...reply, reply: data.reply,date:data.date } 
-                : reply
-            )
-          );
-        })
-        }
+      const EditReply = ()=>{
+        setInputForm(true);
       }
+    const SaveEditReply = (replyId,commentId)=>{
+  if(commentId && replyId){
+    if(replyInput){
+         
+            const updatedReply= {
+              reply: replyInput
+            };
+              fetch(`${baseUrl}/comments/${commentId}/replies/edit/${replyId}`, {
+                method: 'PATCH',
+                headers: {
+                'Content-Type': 'application/json'    
+                },
+                body: JSON.stringify(updatedReply)
+            })
+            .then(response => {
+              if (!response.ok) {
+                throw new Error(`Error: ${response.status}`);
+              }
+              return response.json();
+            })
+            .then(data => {
+              setReplies(prevReplies =>
+                prevReplies.map(reply =>
+                  reply.id === replyId
+                  ? { ...reply, reply: data.reply,date:data.date } 
+                  : reply
+                )
+              );
+          })
+          setInputForm(false);
+          }
+          else{
+            toast.warn("Please Fill Reply Field");
+          }
+         
+        }
         
+    }
     const DeleteReply = async(replyId)=>{
       await fetch(`${baseUrl}/comments/${comment.id}/replies/delete/${replyId}`, {
         method: 'DELETE'
@@ -64,9 +77,13 @@ const Reply = ({reply,loggedInUser,comment,setReplies,setComment}) => {
     .catch(error => console.error('Fetch Error:', error));
     setReplies(prevState => (prevState.filter(el => (el.id !== replyId))))
     }
+    const ReplyChangeHandler = (e)=>{
+      setReplyInput(e.target.value);
+    }
   return (
     <>
-    <div className="ReplyContainer">  
+     <ToastContainer/>
+     <div className="ReplyContainer">  
       <div className="ReplySubContainer">
         <div className="ReplyHeader">
           <Avatar src={user.src}/>
@@ -77,15 +94,17 @@ const Reply = ({reply,loggedInUser,comment,setReplies,setComment}) => {
           </div>
         </div>
         <div className="ReplyBody">
-      <h6 className="ReplyText">{reply.reply}</h6>
+      {!inputForm && <h6 className="ReplyText">{reply.reply}</h6>}
+      {inputForm && <input className="ReplyInput" value={replyInput} onChange={ReplyChangeHandler}></input>}
         </div>
       </div>
-      {(reply.postedBy === loggedInUser.id) && 
+      {(reply.postedBy === loggedInUser.id) && !inputForm &&
       <div className="ButtonContainer">
-      <button className="CardButton EditButton" onClick={()=>EditReply(reply.id,comment.id)}> <FontAwesomeIcon icon={faPen} /></button>
+      <button className="CardButton EditButton" onClick={()=>EditReply()}> <FontAwesomeIcon icon={faPen} /></button>
       <button className="CardButton DeleteButton" onClick={()=>DeleteReply(reply.id)}> <FontAwesomeIcon icon={faTrash} /></button> 
        </div>
       }
+      {(reply.postedBy === loggedInUser.id) && inputForm &&<button className="SaveButton" onClick={()=>SaveEditReply(reply.id,comment.id)}>Edit</button> }
     </div>
     </>
   )
